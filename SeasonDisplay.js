9969c2d493beb4f0c9379605c9970aa5b873b509
Jessica Bassard <53714967+jbassard88@users.noreply.github.com>
2019-10-17 19:47:19 -0700

    Add files via upload

---
+++ SeasonDisplay.js
@@ -0,0 +1,38 @@
+import './SeasonDisplay.css';
+import React from 'react';
+
+const seasonConfig = {
+    summer: {
+        text: "Let's hit the beach!",
+        iconName: 'sun'
+    },
+    winter: {
+        text: "Burr it's cold!",
+        iconName: 'snowflake'
+    }
+};
+
+const getSeason = (lat, month) => {
+    if (month > 2 && month < 9) {
+        return lat > 0 ? 'summer' : 'winter';
+    } else {
+        return lat > 0 ? 'winter' : 'summer';
+    }
+};
+// eslint-disable-next-line no-unused-vars
+const SeasonDisplay = (props) => {
+    const season = getSeason(props.lat, new Date().getMonth());
+    const { text, iconName } = seasonConfig[season];
+
+
+    return (
+        <div className={`season-display ${season}`}>
+            <i className={`icon-left massive ${iconName} icon`} />
+            <h1>{text}</h1>
+            <i className={`icon-right massive ${iconName} icon`} />
+        </div>
+    );
+};
+
+export default SeasonDisplay;
+
